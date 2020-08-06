2cb9a4606bab575e85df72f6819798343e2abdf4
kinan Naem <kinan.berlin@gmail.com>
2020-08-06 15:14:50 +0200

    Completed
    Co-Authored-By: Ghassan Aldarwish <ghassanaldarwish@yahoo.com>
    Co-Authored-By: Mariam Fatima <mariamfatima.co@gmail.com>

---
+++ language number.js
@@ -0,0 +1,33 @@
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+
+console.log(introSentence+" "+ age); // the sum operators let me to add the value of var1 to value of var2
+//*
+let num = 1005;
+console.log(Number(num));
+console.log(typeof (num)); // String
+//
+let num2 = 10.05 ;
+console.log(num2); // it's already Number 
+
+if (30%2 == 0){
+    console.log("30 is even");
+}
+else{
+    console.log("30 is odd");
+}
+
+
+if (939%2 == 0){
+    console.log("939 is even");
+}
+else{
+    console.log("939 is odd");
+}
+
+if (40.9%2 == 0){
+    console.log("40.9 is even");
+}
+else{
+    console.log("40.9 is odd");
+}
