02e2a33507c4cb8b46b3c09e8c339c6673fd07e3
frausafiye <s_gulenc@outlook.com>
2020-08-06 14:22:30 +0200

    added solution except 3

---
+++ safiye/exercise-7.js
@@ -0,0 +1,19 @@
+//1
+let myName = "Safiye";
+let myAge = 29;
+console.log(
+  "hello, my name is " + myName + " and I am " + myAge + " years old."
+);
+//* + operator is to bring different inputs together.  By using + we can concatenate variables or values.
+//2
+//*
+let aString = "1005";
+let aNumber = Number(aString);
+console.log(aNumber);
+console.log(typeof aNumber);
+//*
+let anotherString = "10.05";
+let anotherNumber = Number(anotherString);
+console.log(anotherNumber);
+console.log(typeof anotherNumber);
+//3-ignored
