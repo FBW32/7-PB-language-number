c25b2501e624735e6a3a08252b7422917bccb0bb
arezoofaraji <arezoo.faraji66@gmail.com>
2020-08-06 14:34:03 +0200

    7 done

---
+++ arezoo/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+//concat 2 string
+let result1 = introSentence + " " + age;
+console.log(result1);
+// Q2
+let numberOne = "1005";
+console.log(typeof numberOne);
+let result2 = Number(numberOne);
+console.log(typeof result2);
+
+let numberTwo = "10.5";
+console.log(typeof numberTwo);
+let result3 = Number(numberTwo);
+console.log(typeof result3);
