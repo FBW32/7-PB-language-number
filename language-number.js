fb7688eb8c4f6688ea67c18654291d6cddc4db12
Aiham <aiham.rabee@gmail.com>
2020-08-06 14:52:25 +0200

    ok

---
+++ language-number.js
@@ -0,0 +1,19 @@
+//1
+let myString = "One pluse One = ";
+let myNum = 2;
+console.log(`${myString} ${myNum}`); // One pluse One =  2
+//2
+
+let myValue = "1005";
+let coersedValue = Number(myValue);
+console.log(typeof coersedValue); // number
+
+let myValue1 = "10.05";
+let coersedValue1 = Number(myValue1);
+console.log(typeof coersedValue1); // number
+//3
+// ???
+
+
+
+
