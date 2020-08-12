3538a0b0380ca93eecde472f37816bfe56c359cc
Abayomi <mrbayus2014@gmail.com>
2020-08-12 08:47:05 +0200

    languageNumber

---
+++ bayo/languageNumber.js
@@ -0,0 +1,26 @@
+// 08.08.2020
+// EXERCISE 1 
+//Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.
+let whereAreYou = "I am at block";
+let block = 28;
+console.log(whereAreYou + " " + block); 
+// The sign+ is used to combine the variable to another string.
+
+// EXERCISE 2a
+// Create a variable with the value of "1005". Convert it to a number.
+let myVar = "1005";
+let coersedString = Number(myVar);
+console.log(typeof coersedString);
+
+// EXERCISE 2b
+// Create a variable with the value of "10.05". Convert it to a number.
+let myVar2 = "10.05";
+let coersedStr = Number(myVar2);
+console.log(typeof coersedStr);
+
+
+// EXERCISE 3
+// Check if the following numbers are even numbers. Use a ternary and if the number is even print 30 is even, else print that it is odd.
+// 30
+// 939
+// 40.9
