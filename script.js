f04cec9a5037c10936dab965e317c6e71e9a8cf8
Maja <majamarek.design@gmail.com>
2020-08-14 13:18:54 +0200

    7 done

---
+++ script.js
@@ -0,0 +1,29 @@
+// + operator concatenate those two variables.
+let introSentence = "hello, my name is Fran and I am";
+let age = 25;
+console.log(introSentence + " " + age);
+
+/* Create a variable with the value of "1005". Convert it to a number.
+ */
+let integer = "1005";
+let convertedNum = Number(integer);
+console.log(typeof convertedNum);
+
+/* Create a variable with the value of "10.05". Convert it to a number.
+ */
+let floats = "10.05";
+let convertedNum2 = Number(floats);
+console.log(typeof convertedNum2);
+
+/* Check if the following numbers are even numbers. Use a ternary and if the number is even print 30 is even, else print that it is odd. */
+
+/* let num1 = 30; */
+console.log(30 / 2); //15
+console.log(30 / 3); //10
+console.log(`30 is odd`); // 30  is odd
+console.log(939 / 2); // 469.5
+console.log(939 / 3); // 313
+console.log(`939 is odd`); // 939 is odd
+console.log(40.9 / 2); // 20.45
+console.log(40.9 / 3); // 13.633333333333333
+console.log(Math.round(13.633333333333333)); //  14
